79776ea98e7ddc7c1b24855bff9fe647e1fbafe9
Stefano1990 <stefano.hug@gmail.com>
2020-08-26 15:45:57 +0200

    sent an mqtt message with tortoise

---
+++ mqtt/lib/mqtt/application.ex
@@ -0,0 +1,23 @@
+defmodule Mqtt.Application do
+  use Application
+
+  # def start(_type, _args) do
+  #   IO.puts("application has started")
+
+    # Tortoise.Supervisor.start_child(
+    #   client_id: "my_client_id",
+    #   handler: {Tortoise.Handler.Logger, []},
+    #   server: {Tortoise.Transport.Tcp, host: 'test.mosquitto.org', port: 1883},
+    #   subscriptions: [{"test/channel123", 0}]
+    # )
+
+  #   Tortoise.publish("my_client_id", "test/channel123", "Hello from the World of Tomorrow !", qos: 0)
+  # end
+
+  def start(_type, _args) do
+    children = []
+    opts = [strategy: :one_for_one, name: Mqtt.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+end
